fba896f9d67b7c1a1f71f47d96f5d8e1d51f63a2
Yana Chernenko <yanamyrna@gmail.com>
2026-04-29 12:03:52 +0200

    add task solution fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const inputs = document.querySelectorAll('input');
+const inputs = document.querySelectorAll('form input');
 
 inputs.forEach((input) => {
   const label = document.createElement('label');
@@ -9,9 +9,7 @@ inputs.forEach((input) => {
   label.setAttribute('for', input.id);
   label.textContent = input.name;
   input.parentElement.appendChild(label);
-});
 
-inputs.forEach((input) => {
   input.setAttribute(
     'placeholder',
     input.name.charAt(0).toUpperCase() + input.name.slice(1),
